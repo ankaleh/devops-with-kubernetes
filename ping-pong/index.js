50614755a99a02eef5a59900108ac879deeaab26
Anna Lehto <ankaleh@users.noreply.github.com>
2021-11-12 17:19:56 +0200

    Exercise 1.11

--- ping-pong/index.js
+++ ping-pong/index.js
@@ -2,15 +2,39 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const contextPath = '/pingpong/'
+const path = require('path')
+const fs = require('fs')
+
+const folderName = path.join('/', 'usr', 'src', 'app', 'files')
+const fileToWrite = path.join(folderName, 'pongs.txt')
 
 let counter = 0
 
+const savePongs = () => {
+    try {
+        if (!fs.existsSync(folderName)) {
+          fs.mkdirSync(folderName)
+        }
+    } catch (err) {
+        console.error(err)
+    }
+    try {
+        fs.writeFileSync(fileToWrite, `${counter}`)
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+
 router.get('/', (req, res) => {
+    
     counter += 1
     res.send(`Pong ${counter}`)
+    savePongs()
 })
 
 app.use(contextPath, router)
+
 const port = 8080
 app.listen(port, () => {
     console.log(`Server ready on port ${port}`)
--- writer-reader/reader/index.js
+++ writer-reader/reader/index.js
@@ -6,15 +6,25 @@ const path = require('path')
 
 const folderName = path.join('/', 'usr', 'src', 'app', 'files')
 const filePath = path.join(folderName, 'hash.txt')
+const pathToPongs = path.join(folderName, 'pongs.txt')
 
 app.get('/', (req, res) => {
+    let hash = ''
+    let pongs = 0
     try {
-        const data = fs.readFileSync(filePath, 'utf8')
-        res.send(data)
+        hash = fs.readFileSync(filePath, 'utf8')
       } catch (err) {
         console.error(err)
         res.send('Tiedoston lukeminen ei onnistunut.')
       }
+    try {
+        pongs = fs.readFileSync(pathToPongs, 'utf8')
+      } catch (err) {
+        console.error(err)
+        res.send('Pongs-tiedoston lukeminen ei onnistunut.')
+      }
+      data = hash.concat('.\nPongs: ', pongs)
+      res.send(data)
 })
 
 const port = 5000
