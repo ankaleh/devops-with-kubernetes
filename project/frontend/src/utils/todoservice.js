16c5277742e00400ddd7fee48c32f1b4f0a6836e
Anna Lehto <ankaleh@users.noreply.github.com>
2021-11-17 18:09:03 +0200

    Exercise 2.02: Created a new container for the backend of kub-project

--- project/frontend/src/utils/todoservice.js
+++ project/frontend/src/utils/todoservice.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 
+const baseURL = process.env.REACT_APP_BACKEND_URL
+
+const axiosInstance = axios.create({ baseURL })
 
 export const getTodos = async () => {
     try {
-        const response = await axios.get('http://localhost:8081/api/todos')//await axios.get('http://project-backend-svc:3456/api/todos')
+        const response = await axiosInstance.get('/todos')
         return response
       } catch (error) {
         console.log(error)
@@ -12,7 +15,7 @@ export const getTodos = async () => {
 
 export const postTodo = async (todo) => {
     try {
-        const response = await axios.post('http://localhost:8081/api/todos', todo)//await axios.post('http://project-backend-svc:3456/api/todos', todo)
+        const response = await axiosInstance.post('/todos', todo)//await axios.post('http://project-backend-svc:3456/api/todos', todo)
         console.log(response.data)
         return response
       } catch (error) {
