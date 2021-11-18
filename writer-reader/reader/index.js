a79aabeda3d5a1c09bed867e12cb7e2a84a7df33
Anna Lehto <ankaleh@users.noreply.github.com>
2021-11-18 13:19:13 +0200

    Exercise 2.06: add configMap for reader container

--- writer-reader/reader/index.js
+++ writer-reader/reader/index.js
@@ -2,30 +2,32 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const axios = require('axios')
-
 const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, 'config', '.env') })
 
 const folderName = path.join('/', 'usr', 'src', 'app', 'files')
 const filePath = path.join(folderName, 'hash.txt')
+const message = process.env.MESSAGE
 
+console.log(message)
 app.get('/', async (req, res) => {
     let hash = ''
     let pongs = 0
     try {
         hash = fs.readFileSync(filePath, 'utf8')
-      } catch (err) {
+    } catch (err) {
         console.error(err)
         res.send('Tiedoston lukeminen ei onnistunut.')
-      }
-      try {
-        const response = await axios.get('http://ping-pong-svc:2345/pingpong')
-        pongs = response.data
-      } catch (error) {
+    }
+    try {
+      const response = await axios.get('http://ping-pong-svc:2345/pingpong')
+      pongs = response.data
+    } catch (error) {
         console.log(error)
-      }
+    }
       
-      data = hash.concat('.\nPongs: ', pongs)
-      res.send(data)
+    data = message.concat('<br/>', hash, '.<br/>Pongs: ', pongs)
+    res.send(data)
 })
 
 const port = 5000
